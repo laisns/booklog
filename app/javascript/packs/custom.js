6e52450a8dfed3b8fcc3f6d851e72ba0f8da1ff5
Laís <lais.sdsn@gmail.com>
2020-12-10 18:37:01 -0300

    added custom checkbox and custom alert messages

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -11,43 +11,60 @@ $(document).ready(function (){
         $(this).closest('.toast').toast('hide');
     });
 
+    $('.favorite-checkbox').on('click', function (e){
+        toggleFavoriteIcon($(this))
+    });
+
 
 });
 
 function autoCompleteBookTitle(input){
-    // autoCompleteInput.on('load change paste keyup', function () {
-        var fieldValue = input;
-        var options = {
-            url: function(phrase) {
-                return "/books/search.json?q=" + phrase;
+    var fieldValue = input;
+    var options = {
+        url: function(phrase) {
+            return "/books/search.json?q=" + phrase;
+        },
+        getValue: function(element) {
+            return element.title;
+        },
+        ajaxSettings: {
+            dataType: "json",
+            method: "GET",
+            data: {
+                dataType: "json"
+            }
+        },
+        preparePostData: function (data) {
+            data.phrase = $("input#name").val();
+            return data;
+        },
+        requestDelay: 900,
+        list: {
+            match: {
+                enabled: true
             },
-            getValue: function(element) {
-                return element.title;
+            onSelectItemEvent: function () {
+                var selectedItem = input.getSelectedItemData();
+                $("input#book_id").val(selectedItem.id);
+                fieldValue.val(selectedItem.title);
             },
-            ajaxSettings: {
-                dataType: "json",
-                method: "GET",
-                data: {
-                    dataType: "json"
-                }
-            },
-            preparePostData: function (data) {
-                data.phrase = $("input#name").val();
-                return data;
-            },
-            requestDelay: 900,
-            list: {
-                match: {
-                    enabled: true
-                },
-                onSelectItemEvent: function () {
-                    var selectedItem = input.getSelectedItemData();
-                    $("input#book_id").val(selectedItem.id);
-                    fieldValue.val(selectedItem.title);
-                },
-            },
-            theme: "plate-dark",
-        };
-        $("[data-behavior='autocomplete']").easyAutocomplete(options);
-    // });
+        },
+        theme: "plate-dark",
+    };
+    $("[data-behavior='autocomplete']").easyAutocomplete(options);
+};
+
+function toggleFavoriteIcon(element) {
+    var checkbox = element.children('input[type=checkbox]')[0];
+    var iconChecked = element.children('.icon-checked');
+    var iconNonChecked = element.children('.icon-non-checked');
+    if (checkbox.checked) {
+        checkbox.checked = false;
+        iconNonChecked.removeAttr('hidden');
+        iconChecked.attr('hidden', 'hidden')
+    } else {
+        checkbox.checked = true;
+        iconChecked.removeAttr('hidden');
+        iconNonChecked.attr('hidden', 'hidden')
+    }
 };
